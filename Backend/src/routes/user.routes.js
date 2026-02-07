ccc7540a4f879eb06d53c4769111ba21ea3cd728
Saksham Khaire <sakshamkhaire2007@gmail.com>
2026-02-07 12:38:17 +0530

    Updated the /me route to fetch the full user profile

--- Backend/src/routes/user.routes.js
+++ Backend/src/routes/user.routes.js
@@ -2,15 +2,14 @@ import express from "express";
 import authMiddleware from "../middleware/auth.middleware.js";
 import roleMiddleware from "../middleware/role.middleware.js";
 import { createStudentCoordinator } from "../controllers/user.controller.js";
+import User from "../models/User.model.js";
 
 const router = express.Router();
 
-router.get("/me", authMiddleware, (req, res)=>{
-  res.status(200).json({
-  message: "User profile fetched",
-  user: req.user
+router.get("/me", authMiddleware, async (req, res) => {
+  const user =  await User.findById(req.user.userId).select("-password");
+  res.json({success: true, user});
 });
-})
 
 router.post(
   "/create-student-coordinator",
